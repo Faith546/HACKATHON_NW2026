52ad0870845cf3dd46a2b9413ff70d6b1faca950
Gabriel <gabochu280@gmail.com>
2026-08-30 01:32:22 -0600

    Revert "feat(carriers): add carrier reactivation endpoint"
    This reverts commit ce0060f64db0c487b182f4c1dafb127c12a9358f.

--- src/modules/carriers/carriers.routes.ts
+++ src/modules/carriers/carriers.routes.ts
@@ -6,5 +6,4 @@ export const carriersRouter = Router();
 
 carriersRouter.get("/", asyncHandler(carriersController.list));
 carriersRouter.post("/", asyncHandler(carriersController.create));
-carriersRouter.post("/:carrierId/activate", asyncHandler(carriersController.activate));
 carriersRouter.delete("/:carrierId", asyncHandler(carriersController.remove));
